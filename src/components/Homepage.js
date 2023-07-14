6b4f97b8f67dffca1cf4a466d935472ee2275fb2
bamithaxioned <amithkumarb@axioned.com>
2023-07-14 14:39:42 +0530

    Section added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -50,6 +50,12 @@ const Homepage = () => {
 
   return (
     <>
+      <section className="homepage">
+        <div className="wrapper flex items-center justify-center text-center flex-col py-5">
+          <h2 className='uppercase font-bold text4xl tracking-wide mb-5 md:text-6xl lg:text-8xl'>Teen Blog</h2>
+          <Link to={'/blog'} className='py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold'>Visit Blog Page</Link>
+        </div>
+      </section>
       {data.content?.length && data.content?.map((section) => {
           let type = section.type;
           let id = section.id;
@@ -58,14 +64,7 @@ const Homepage = () => {
           } else if(type === 'listingcategory') {
             return <ListingCategory data={section} key={id} />
           }
-        }) 
-        // <section className="homepage">
-        //   <div className="wrapper flex items-center justify-center text-center h-screen flex-col">
-        //     <h2 className='uppercase font-bold text4xl tracking-wide mb-5 md:text-6xl lg:text-8xl'>Teen Blog</h2>
-        //     <Link to={'/blog'} className='py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold'>Visit Blog Page</Link>
-        //   </div>
-        // </section>
-      }
+        })}
     </>
   )
 }
